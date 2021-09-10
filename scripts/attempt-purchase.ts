b2b954ac0f92c3f04a75659f9c252e2791c9fef1
sbmsr <sebastian.messier@gmail.com>
2021-09-10 13:22:15 -0400

    cleanup imports

--- scripts/attempt-purchase.ts
+++ scripts/attempt-purchase.ts
@@ -1,9 +1,9 @@
-import hre, { ethers } from "hardhat";
+import { ethers } from "hardhat";
 import { Medici } from "../typechain/index";
 
 async function main() {
   let Medici: Medici;
-  Medici = (await hre.ethers.getContractAt("Medici", "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0")) as Medici;
+  Medici = (await ethers.getContractAt("Medici", "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0")) as Medici;
   console.log(await Medici.attemptPurchase(["1"], { value: ethers.utils.parseEther("0.01") }));
 }
 
